ffbc28bc2e5896015bca47fff5bb037cdbb46f93
MitchelleTamnai <cheptoektamnai@gmail.com>
2024-03-18 10:38:42 +0300

    Completed Lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+const currentUser = 'Tamnai Mitchelle'
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`
+
+const upperCaseCurrentUser = currentUser.toUpperCase()
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${upperCaseCurrentUser}!`
+
+
+const firstInitial = currentUser[0]
+const shortGreeting = `Welcome, ${firstInitial}!`
